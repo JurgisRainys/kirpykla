edaf4ae7c1a314627abd4cf356a318522762717f
Mindaugas <budr.mindaugas@gmail.com>
2018-09-26 18:21:34 +0200

    kazkas va

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,16 +6,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
+import { ShopServicesComponent } from './pages/shop-services/shop-services.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'rezervacija', component: ReservationComponent},
+  { path: 'paslaugos', component: ShopServicesComponent},
+  { path: 'kontaktai', component: ContactsComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ReservationComponent,
+    ShopServicesComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
